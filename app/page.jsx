03fd1a1579d8deaa48c6e5570fae7e855548d32c
wale2009 <adewaleadebayo202@gmail.com>
2024-05-20 16:02:54 +0100

    Added icons

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import Hero from "@/components/Hero";
 import Menu from "@/components/Menu";
 import Reservation from "@/components/Reservation";
 import About from "@/components/About";
-// import Map from "@/components/Map";
 import Footer from "@/components/Footer";
 
 
@@ -17,10 +16,7 @@ export default function Home() {
       <Menu />
       <Reservation />
       <About />
-      {/* <Map /> */}
       <Footer />
-      {/* <StyleGuide /> */}
-      {/* <div className="h-[4000px]"></div> */}
     </main>
   );
 }
